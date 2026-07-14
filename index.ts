6c2b0b8595cacbf6b459fe5b6d8c182e6f6d80e7
Ibrahim Khalilullah <01754488189ib@gmail.com>
2026-07-14 15:58:59 +0600

    feat: configure cors options and integrate cookieParser middleware

--- index.ts
+++ index.ts
@@ -1,12 +1,32 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+  }),
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is up and running!");
